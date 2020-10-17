38bf8d55b543e48e59245be6d31565a8857fae9c
Pablo Trabajos <hz302783@hertz.net>
2020-10-17 18:55:33 +0100

    Initial Commit

---
+++ Landmark-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmark-iOS
+//
+//  Created by Pablo Tamayo on 17/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmark-iOS/Landmark_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark_iOSApp.swift
+//  Landmark-iOS
+//
+//  Created by Pablo Tamayo on 17/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Landmark_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
