495f26942b4a32bb24edf58404838e4d0c3a6b04
Pablo Trabajos <hz302783@hertz.net>
2021-03-07 20:16:30 +0000

    Add view containing map, improve main SwiftUi view

--- Landmark-iOS/ContentView.swift
+++ Landmark-iOS/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Landmark-iOS
+//  Landmark
 //
 //  Created by Pablo Tamayo on 17/10/2020.
 //
@@ -9,10 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+//                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .font(.title)
+                    .foregroundColor(.green)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                }
+            }
             .padding()
+            
+            Spacer()
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
